e7095759ee2de80d922e2b0c7db0eda6e32dbb05
Paul Fleischer-Djoleto <pfleischer2002@yahoo.co.uk>
2022-01-12 22:17:34 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-const _ = require('lodash')
+var http = require('http')
+var fs = require('fs')
 
-const items = [1,[2, [3, [4]]]]
-const newIems = _.flattenDeep(items)
+http
+  .createServer(function (req, res) {
+    /*   const text = fs.readFileSync('./content/big.txt', 'utf8')
+    res.end(text) */
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
+    fileStream.on('open', () => {
+      fileStream.pipe(res)
+    })
+    fileStream.on('error', (err) => res.end(err))
+  })
+  .listen(5000)
 
-console.log(newIems);
+/* const { createReadStream } = require('fs')
+
+const stream = createReadStream('./content/big.txt', {
+  highWaterMark: 90000,
+  encoding: 'utf8',
+})
+
+stream.on('data', (result) => {
+  console.log(result)
+})
+
+stream.on('error', (err) => console.log(err)) */
